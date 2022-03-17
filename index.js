c45e7684dd7096fe8c2b20d164acdf0fe0d1f193
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-03-17 16:52:43 -0700

    postman setting

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
-// 6:31~
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+  app.use(express.json());
+  app.use("/api/users", userRoute);
+
+
+
+
+
+
+app.listen(process.env.PORT || 5001, () => {
+  console.log("Backend server is running!");
+});
+
+/// Control Center stops listening to 5000 & 7000 ports when I turn off “AirPlay Receiver” in the “Sharing” System Preference////
