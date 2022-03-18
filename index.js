444c17cb5c65f6c17a5491f582a8099eb1c951c8
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-03-18 00:59:20 -0700

    jwt

--- index.js
+++ index.js
@@ -3,23 +3,20 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfull!"))
+  .then(() => console.log("DB Connection Successful!"))
   .catch((err) => {
     console.log(err);
   });
 
-  app.use(express.json());
-  app.use("/api/users", userRoute);
-
-
-
-
-
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5001, () => {
   console.log("Backend server is running!");
---
+++ routes/auth.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      userName: req.body.user_name,
+    });
+    !user && res.status(401).json("Wrong User Name");
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    OriginalPassword != req.body.password &&
+      res.status(401).json("Wrong Password");
+    const { password, ...others } = user._doc;
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    res.status(201).json({ others, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+
+///54:12~
