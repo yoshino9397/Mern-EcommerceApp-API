b97fc78ec52e80152dc68e139609d1cf29ceb501
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-03-19 00:56:34 -0700

    token add

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,56 @@
 const User = require("../models/User");
 const router = require("express").Router();
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
-// router.get('/usertest',(req, res) =>{
-//   res.send("usertest is successful")
-// })
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
 
-// router.post('/userposttest',(req, res) =>{
-//   const username = req.body.username
-// res.send("Your name is " + username)
-// })
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//1:13:15~
+//GET ALL USER
 
 module.exports = router;
