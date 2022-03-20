ce47dff23fcfa307f2a5e4754b5326b34a7cf88d
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-03-19 17:13:00 -0700

    added product API

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
-
+const productRoute = require("./routes/product");
 dotenv.config();
 
 mongoose
@@ -17,6 +17,8 @@ mongoose
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+
 
 app.listen(process.env.PORT || 5001, () => {
   console.log("Backend server is running!");
