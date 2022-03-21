88492ccdb092a8b98a628f2099eecd59c5441f6d
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-03-21 16:55:54 -0700

    stripe fix

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 dotenv.config();
 
 mongoose
@@ -16,12 +18,14 @@ mongoose
     console.log(err);
   });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5001, () => {
   console.log("Backend server is running!");
